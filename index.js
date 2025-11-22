e0d341d75170d898f41658300daf3c62777192e4
Daniel-wambua <wambuahdaniel@gmail.com>
2025-11-22 13:36:40 +0300

    simple tweak

--- index.js
+++ index.js
@@ -6,20 +6,37 @@ import random from "random";
 const path = "./data.json";
 
 
+
 const makeCommits = (n) => {
   if (n === 0) return simpleGit().push();
   // Pick a random day within the last 365 days
   const daysAgo = random.int(0, 364);
   const date = moment().subtract(daysAgo, "days").format();
-  // Write a unique value to data.json
-  const data = {
-    date: date,
-    value: Math.random().toString(36).substring(2) // unique random string
+  // Pick a random number of commits for this day (1-10)
+  const commitsToday = random.int(1, 10);
+  let commitsDone = 0;
+
+  const commitForDay = () => {
+    if (commitsDone >= commitsToday) {
+      // Move to next day/iteration
+      makeCommits(n - 1);
+      return;
+    }
+    // Write a unique value to data.json
+    const data = {
+      date: date,
+      value: Math.random().toString(36).substring(2) + '-' + commitsDone // unique random string
+    };
+    console.log(`Committing for date: ${date} (commit ${commitsDone + 1} of ${commitsToday})`);
+    jsonfile.writeFile(path, data, () => {
+      simpleGit().add([path]).commit(`Commit for ${date} #${commitsDone + 1}`, { "--date": date }, () => {
+        commitsDone++;
+        commitForDay();
+      });
+    });
   };
-  console.log(`Committing for date: ${date}`);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(`Commit for ${date}`, { "--date": date }, makeCommits.bind(this, --n));
-  });
+
+  commitForDay();
 };
 
 makeCommits(200);
